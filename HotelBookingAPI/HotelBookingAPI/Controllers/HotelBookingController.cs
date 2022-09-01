39d26406df673647a633de96de230803e2af052d
raffaele <raff.iuliani@gmail.com>
2022-08-31 22:14:51 -0400

    Created an API to book hotel rooms via an API with in memory database.

---
+++ HotelBookingAPI/HotelBookingAPI/Controllers/HotelBookingController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelBookingAPI.Models;
+using HotelBookingAPI.Data;
+
+namespace HotelBookingAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class HotelBookingController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        
+        public HotelBookingController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public JsonResult CreateEdote(HotelBooking booking)
+        {
+            if(booking.Id == 0)
+            {
+                _context.Bookings.Add(booking);
+            }
+            else
+            {
+                var bookingInDb = _context.Bookings.Find(booking.Id);
+
+                if (bookingInDb == null)
+                {
+                    return new JsonResult(NotFound());
+                }
+                else
+                {
+                    bookingInDb = booking;
+                }
+            }
+
+            _context.SaveChanges();
+
+            return new JsonResult(Ok(booking));    
+        }
+
+        [HttpGet]
+        public JsonResult Get(int id)
+        {
+            var result = _context.Bookings.Find(id);
+
+            if (result == null)
+                return new JsonResult(NotFound());
+
+            return new JsonResult(Ok(result));
+        }
+
+
+        [HttpDelete]
+        public JsonResult Delete(int id)
+        {
+            var result = _context.Bookings.Find(id);
+
+            if (result == null)
+                return new JsonResult(NotFound());
+            else
+                _context.Remove(result);
+            _context.SaveChanges();
+
+            return new JsonResult(NoContent());
+        }
+
+        [HttpGet()]
+        public JsonResult GetAll()
+        {
+            var result = _context.Bookings.ToList();
+
+            return new JsonResult(Ok(result));
+        }
+    }
+}
